e334fbb7e894dbb90f1720260331747cb696ad03
GueyLing <glhneah@gmail.com>
2024-05-19 10:24:28 +0800

    fe middleware route done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,15 +9,16 @@ import { RegisterComponent } from './register/register.component';
 import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
 import { PlayerDetailComponent } from './player-detail/player-detail.component';
 import { AdminGuard } from './admin.guard';
+import { UserGuard } from './user.guard';
 
 const appRoutes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'crud_players', component: CrudPlayersComponent, canActivate: [AdminGuard] },
-  { path: 'add-player', component: AddPlayerComponent },
-  { path: 'edit-player/:id', component: EditPlayerComponent },
+  { path: 'add-player', component: AddPlayerComponent, canActivate: [AdminGuard] },
+  { path: 'edit-player/:id', component: EditPlayerComponent, canActivate: [AdminGuard] },
   { path: 'register', component: RegisterComponent },
-  { path: 'user_dashboard', component: UserDashboardComponent},
-  { path:'player_details/:id', component: PlayerDetailComponent}
+  { path: 'user_dashboard', component: UserDashboardComponent, canActivate: [UserGuard]},
+  { path:'player_details/:id', component: PlayerDetailComponent, canActivate: [UserGuard]}
 ];
 
 @NgModule({
